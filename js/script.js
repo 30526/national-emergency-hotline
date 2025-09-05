f9d7ffbe63e41d1175690c9b5e004756b78e4df4
30526 <taiburrahamanapu@gmail.com>
2025-09-05 23:40:48 +0600

    added call button javascript function

--- js/script.js
+++ js/script.js
@@ -50,3 +50,109 @@ document.getElementById('heart-button-9')
     .addEventListener('click', function () {
         heartButton('heart-button-9')
     })
+
+// Heart button function ended 
+// ---------------------------------------------------------------------
+
+// call button function 
+function callButton(id) {
+    const coinAmount = parseInt(document.getElementById('coin-amount').innerText)
+    if (coinAmount < 20) {
+        alert('Insufficient coin, please recharge!')
+        return false;
+    }
+    const newCoinAmount = coinAmount - 20
+    document.getElementById('coin-amount').innerText = newCoinAmount
+    return true;
+}
+
+
+// 1
+document.getElementById('call-btn-1')
+    .addEventListener('click', function () {
+
+        const completed = callButton('call-btn-1')
+        if (completed === true) {
+            alert('Calling National Emergency 999')
+        }
+    })
+
+// 2
+document.getElementById('call-btn-2')
+    .addEventListener('click', function () {
+
+        const completed = callButton('call-btn-2')
+        if (completed === true) {
+            alert('Calling National Police 999')
+        }
+    })
+
+// 3
+document.getElementById('call-btn-3')
+    .addEventListener('click', function () {
+
+        const completed = callButton('call-btn-3')
+        if (completed === true) {
+            alert('Calling Fire Service 999')
+        }
+    })
+
+// 4
+document.getElementById('call-btn-4')
+    .addEventListener('click', function () {
+
+        const completed = callButton('call-btn-4')
+        if (completed === true) {
+            alert('Calling Ambulance 1994-999999')
+        }
+    })
+
+// 5
+document.getElementById('call-btn-5')
+    .addEventListener('click', function () {
+
+        const completed = callButton('call-btn-5')
+        if (completed === true) {
+            alert('Calling Women & Child Helpline 109')
+        }
+    })
+
+// 6
+document.getElementById('call-btn-6')
+    .addEventListener('click', function () {
+
+        const completed = callButton('call-btn-6')
+        if (completed === true) {
+            alert('Calling Anti-Corruption 106')
+        }
+    })
+
+// 7
+document.getElementById('call-btn-7')
+    .addEventListener('click', function () {
+
+        const completed = callButton('call-btn-7')
+        if (completed === true) {
+            alert('Calling Electricity Outage 16216')
+        }
+    })
+
+// 8
+document.getElementById('call-btn-8')
+    .addEventListener('click', function () {
+
+        const completed = callButton('call-btn-8')
+        if (completed === true) {
+            alert('Calling Brac 16445')
+        }
+    })
+
+// 9
+document.getElementById('call-btn-9')
+    .addEventListener('click', function () {
+
+        const completed = callButton('call-btn-9')
+        if (completed === true) {
+            alert('Calling Bangladesh Railway 163')
+        }
+    })
